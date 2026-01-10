6649f966a8b626b798f9e21e6703a216c3240c25
ttthuan <ttthuan.22.fwo@gmail.com>
2026-01-10 19:49:02 +0700

    fix nodemailer

--- backend/dist/configs/nodeMailer.js
+++ backend/dist/configs/nodeMailer.js
@@ -6,20 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const env_1 = require("../configs/env");
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const transporter = nodemailer_1.default.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
         user: env_1.ENV.EMAIL_USER,
         pass: env_1.ENV.EMAIL_PASS,
-    }
+    },
 });
 transporter.verify((error, success) => {
     if (error) {
-        console.error('Email config error. Check HOST, PORT, and APP PASSWORD:', error.message);
+        console.error("Email config error. Check HOST, PORT, and APP PASSWORD:", error.message);
     }
     else {
-        console.log('Email server is ready. Using host:', 465);
+        console.log("Email server is ready. Using host:", 465);
     }
 });
 exports.default = transporter;
--- backend/src/configs/nodeMailer.ts
+++ backend/src/configs/nodeMailer.ts
@@ -1,19 +1,20 @@
 import { ENV } from "../configs/env";
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: ENV.EMAIL_USER,
-        pass: ENV.EMAIL_PASS,
-    }
+  service: "gmail",
+  auth: {
+    user: ENV.EMAIL_USER,
+    pass: ENV.EMAIL_PASS,
+  },
 });
 transporter.verify((error, success) => {
-    if (error) {
-        console.error('Email config error. Check HOST, PORT, and APP PASSWORD:', error.message);
-    } else {
-        console.log('Email server is ready. Using host:', 465);
-    }
+  if (error) {
+    console.error(
+      "Email config error. Check HOST, PORT, and APP PASSWORD:",
+      error.message
+    );
+  } else {
+    console.log("Email server is ready. Using host:", 465);
+  }
 });
-export default transporter
+export default transporter;
